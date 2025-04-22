638ed1dcefebbeb5698046e357790a7f317dd65c
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2025-04-22 19:14:50 +0200

    refactor: update OnRun event types to allow any return type

--- src/types/OnRunEvent.ts
+++ src/types/OnRunEvent.ts
@@ -5,6 +5,6 @@ export type OnRunProps = OnRunPropsOverride;
 /**
  * The function that is called when the class is runned.
  */
-export type OnRunEvent<T> = (item: T, props: OnRunProps) => void | Promise<void>;
-export type OnRunFunction = (props: OnRunProps) => void | Promise<void>;
-export type OnRunAsyncFunction = (props: OnRunProps) => Promise<void>;
+export type OnRunEvent<T> = (item: T, props: OnRunProps) => any | Promise<any>;
+export type OnRunFunction = (props: OnRunProps) => any | Promise<any>;
+export type OnRunAsyncFunction = (props: OnRunProps) => Promise<any>;
