b3d36fa2f6d3d63bd1cc1df0f59f97a6c015890c
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-02-12 09:44:41 +0100

    feat: update version to 0.7.0; add new entry for ink in tsup configuration and enhance module exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -44,6 +44,26 @@ export default defineConfig([
     {
         target: "es2020",
         entry: {
+            ink: "src/ink/index.ts",
+        },
+        format: ["cjs", "esm"], // Build for commonJS and ESmodules
+        dts: true, // Generate declaration file (.d.ts)
+        treeshake: true,
+        // sourcemap: true, // Generate sourcemap, it was removed because otherwise it would explode
+        clean: true,
+        minify: true,
+        bundle: true,
+        skipNodeModulesBundle: false, // Skip bundling of node_modules
+        external: ["@drincs/pixi-vn", "@drincs/nqtr/registries", "@drincs/nqtr/handlers"],
+        outExtension({ format }) {
+            return {
+                js: format === "esm" ? ".mjs" : ".cjs",
+            };
+        },
+    },
+    {
+        target: "es2020",
+        entry: {
             index: "src/index.ts",
         },
         format: ["cjs", "esm"], // Build for commonJS and ESmodules
