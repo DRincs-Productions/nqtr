6ffccfd67bfe060e2067456db4af549ca36dea83
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-03-06 19:28:05 +0100

    refactor: update commitment handling in nqtrHandler and enhance RoomInterface with addCommitment and removeCommitment methods

--- src/interface/navigation/RoomInterface.ts
+++ src/interface/navigation/RoomInterface.ts
@@ -2,6 +2,8 @@ import { RoomInterface as OverrideRoomInterface } from "@drincs/nqtr";
 import type { CharacterInterface } from "@drincs/pixi-vn";
 import { CommitmentInterface, LocationInterface as LocationInterfaceInt } from "..";
 import { OnRunAsyncFunction } from "../../types";
+import DateSchedulingInterface from "../DateSchedulingInterface";
+import TimeSchedulingInterface from "../TimeSchedulingInterface";
 import NavigationAbstractInterface from "./NavigationAbstractClass";
 
 export default interface RoomInterface extends RoomBaseInternalInterface, OverrideRoomInterface {}
@@ -15,6 +17,31 @@ export interface RoomBaseInternalInterface extends NavigationAbstractInterface {
      * The location where the room is.
      */
     readonly location: LocationInterfaceInt;
+    /**
+     * Connects the commitment to the class.
+     * @param commitment The commitment to connect to the class.
+     * @param options
+     * @returns
+     */
+    addCommitment(
+        commitment: CommitmentInterface,
+        options?: {
+            /**
+             * Time slot in which commitment will be active.
+             */
+            timeSlot?: TimeSchedulingInterface;
+            /**
+             * Used to schedule what date it will be added and removed.
+             */
+            dateScheduling?: DateSchedulingInterface;
+        },
+    ): void;
+    /**
+     * Disconnects the commitment from the class.
+     * @param commitment The commitment to disconnect from the class.
+     * @param options
+     */
+    removeCommitment(commitment: CommitmentInterface | string, options?: Pick<DateSchedulingInterface, "to">): void;
     /**
      * Get the character commitments of the room.
      */
